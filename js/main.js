83c5bebbef7eebcbcb6e51d6d2690070ba597ac9
abby-garcia <acg361@gmail.com>
2015-10-06 16:30:26 -0400

    added buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+$( document ).ready(function() {
+    $('.red').on("click", function() {
 
+
+		$('.body-content').css({'background-color': '#D00000'});
+
+	});
+
+
+
+$('.blue').on("click", function() {
+    		
+
+		$('.body-content').css({'background-color': '#3333FF'});
+
+	});
+
+$('.pink').on("click", function() {
+    		
+
+		$('.body-content').css({'background-color': '#FF2BB2'});
+
+	});
+
+$('.green').on("click", function() {
+    		
+
+		$('.body-content').css({'background-color': '#32F999'});
+
+	});
+
+
+
+
+
+
+});
